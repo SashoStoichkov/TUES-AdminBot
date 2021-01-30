88e40e009a18a29ce2c613ef9af5815d101df7a6
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-01-30 17:41:15 +0200

    send dm on kick

--- admin_bot.py
+++ admin_bot.py
@@ -31,6 +31,15 @@ def get_members_with_role(guild, role):
 async def remove_all_roles(member):
     await member.edit(roles=[])
 
+async def kick_and_dm(member):
+    dm = await member.create_dm()
+
+    # TODO: Think of kick dm
+    # await dm.send(f'Hello @{member.name}')
+    # await dm.send('You have been kicked from the server. Have a nice life :)')
+
+    await member.kick(reason='TUES IS OVER.')
+
 async def update_roles(guild, old_role, new_role):
     members = get_members_with_role(guild, old_role)
 
@@ -75,7 +84,7 @@ async def update_alumni(guild):
     alumni = get_members_with_role(guild, role_12)
     for student in alumni:
         await remove_all_roles(student)
-        await student.kick(reason='TUES IS OVER.')
+        await kick_and_dm(student)
 
 async def update(guild):
     await update_admins(guild)
