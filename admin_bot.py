7a8b1af8edf6f33f6be1c3ab9298c0d428fda258
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-02-01 11:17:00 +0200

    Bot is ready

--- admin_bot.py
+++ admin_bot.py
@@ -38,9 +38,9 @@ async def remove_all_roles(member):
 async def kick_and_dm(member):
     dm = await member.create_dm()
 
-    # TODO: Think of kick dm
-    await dm.send(f'Здравей, {member.mention}')
-    await dm.send('You have been kicked from the server. Have a nice life :)')
+    await dm.send(f':wave: Здравей, {member.mention}')
+    await dm.send(':tada: Честито завършване!')
+    await dm.send(':star2: С пожелание за успехи, както в професионален, така и в личен план!')
 
     await member.kick(reason='TUES IS OVER.')
 
@@ -87,8 +87,8 @@ async def update_students(guild, bot_log):
     }
 
     for old_role, new_role in roles.items():
-        await bot_log.send(f'{old_role.mention}, добре дошли в {new_role.mention}')
         await update_roles(guild, old_role, new_role)
+        await bot_log.send(f'{old_role.mention}, добре дошли в {new_role.mention}')
 
 async def update_alumni(guild):
     role_12 = get(guild.roles, name='12ти клас')
@@ -117,13 +117,6 @@ async def roles_update():
 
 # ----------------------------------------------------------------
 
-async def test():
-    guild = get(bot.guilds, name=GUILD)
-    bot_log = get(guild.channels, name='bot-log')
-
-    await bot_log.send('Test on specific date!')
-    await bot_log.send('Today should be 01.02.2021 12:00')
-
 @bot.event
 async def on_ready():
     guild = get(bot.guilds, name=GUILD)
@@ -131,12 +124,11 @@ async def on_ready():
     print(
         f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})\n'
-        f'*** Bot is ready ***'
+        f'*** Bot is up-and-running ***'
     )
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(test, CronTrigger.from_crontab('30 8 1 FEB *')) # 10:30
-    # scheduler.add_job(roles_update, CronTrigger.from_crontab('0 12 15 SEP *'))
+    scheduler.add_job(roles_update, CronTrigger.from_crontab('0 8 15 SEP *')) # 10:00
     scheduler.start()
 
 if __name__ == '__main__':
