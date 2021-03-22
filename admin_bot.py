0b97251b50265ed6016a151f73056f76be8f07be
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-03-22 19:22:48 +0200

    Update admin_bot.py

--- admin_bot.py
+++ admin_bot.py
@@ -21,7 +21,11 @@ async def roles_update():
 
     await bot_log.send('Starting Update!')
 
+    await bot_log.send('Благодарим на Админите:')
     await update.update_admins(guild, bot_log)
+
+    await bot_log.send('HackTUES Update')
+    await update.update_hacktues(guild)
 
     await bot_log.send('Казваме ДОВИЖДАНЕ на 12ти клас! :wave:')
     await update.update_alumni(guild)
@@ -43,34 +47,36 @@ async def on_ready():
         f'*** Bot is up-and-running ***'
     )
 
+    await roles_update()
+
     scheduler = AsyncIOScheduler()
     scheduler.add_job(roles_update, CronTrigger.from_crontab('0 8 15 SEP *')) # 10:00
     scheduler.start()
 
 @slash.slash(
-  name="setup",
-  description="Получаване на роли и nickname",
-  options=[
-    manage_commands.create_option(
-        name = "ClassNumber",
-        description = "Кой клас си? 8-12",
-        option_type = 3,
-        required = True
-    ),
-    manage_commands.create_option(
-        name = "ClassLetter",
-        description = "Паралелка А-Г (главни букви на кирилица)",
-        option_type = 3,
-        required = True
-    ),
-    manage_commands.create_option(
-        name = "Name",
-        description = "Име и фамилия на кирилица",
-        option_type = 3,
-        required = True
-    )
-  ],
-  guild_ids=[797018153406300160]
+    name='setup',
+    description='Получаване на роли и nickname',
+    options=[
+        manage_commands.create_option(
+            name = 'ClassNumber',
+            description = 'Кой клас си? 8-12',
+            option_type = 3,
+            required = True
+        ),
+        manage_commands.create_option(
+            name = 'ClassLetter',
+            description = 'Паралелка А-Г (главни букви на кирилица)',
+            option_type = 3,
+            required = True
+        ),
+        manage_commands.create_option(
+            name = 'Name',
+            description = 'Име и фамилия на кирилица',
+            option_type = 3,
+            required = True
+        )
+    ],
+    guild_ids=[797018153406300160]
 )
 async def _setup(ctx, number: str, letter: str, name: str):
     if number not in ['8', '9', '10', '11', '12'] or letter not in ['А', 'Б', 'В', 'Г']:
@@ -93,6 +99,36 @@ async def _setup(ctx, number: str, letter: str, name: str):
 
         await member.add_roles(all_role, roles[number])
         await member.edit(nick=f'{name} ({number}{letter})')
+
+@slash.slash(
+    name='ht',
+    description='Получаване на роля и nickname (HackTUES)',
+    options=[
+        manage_commands.create_option(
+            name = 'Name',
+            description = 'Име и фамилия на кирилица',
+            option_type = 3,
+            required = True
+        )
+    ],
+    guild_ids=[797018153406300160]
+)
+async def _ht(ctx, name: str):
+    await ctx.respond()
+
+    member = ctx.author
+    guild = ctx.message.guild
+
+    ht_role = get(guild.roles, name='HackTUES')
+    role_11 = get(guild.roles, name='11ти клас')
+
+    await member.add_roles(ht_role, role_11)
+    await member.edit(nick=f'{name}')
+
+@slash.slash(name='ping', guild_ids=[797018153406300160])
+async def _ping(ctx):
+    await ctx.respond()
+    await ctx.send(f'Pong! ({bot.latency*1000}ms)')
 
 if __name__ == '__main__':
     bot.run(utils.TOKEN, bot=True)
